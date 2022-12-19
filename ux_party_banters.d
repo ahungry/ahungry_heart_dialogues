a01e10bde3343456f157bc3e3668040e7889c295
Matthew Carter <m@ahungry.com>
2022-12-18 21:19:19 -0500

    Add more banters

--- ux_party_banters.d
+++ ux_party_banters.d
@@ -1,36 +1,103 @@
-CHAIN IF ~Global("ux_banter_1", "GLOBAL", 0) !StateCheck("uxzar", CD_STATE_NOTVALID) InParty("uxzar") See("uxzar") !StateCheck("uxzar", CD_STATE_NOTVALID) InParty("uxana") See("uxana") !StateCheck("uxana", CD_STATE_NOTVALID) InParty("uxolr") See("uxolr") !StateCheck("uxolr", CD_STATE_NOTVALID)~ THEN uxbzar ux_lbl_chain_1
-@000261 DO ~SetGlobal("ux_banter_1", "GLOBAL", 1) ~
-  == uxbzar @000261 DO ~~
-  == uxbana @000262 DO ~~
-  == uxbolr @000263 DO ~~
+CHAIN IF ~Global("ux_banter_1", "GLOBAL", 0) !StateCheck("uxzar", CD_STATE_NOTVALID) InParty("uxzar") See("uxzar") !StateCheck("uxzar", CD_STATE_NOTVALID) InParty("uxolr") See("uxolr") !StateCheck("uxolr", CD_STATE_NOTVALID)~ THEN uxbzar ux_lbl_chain_1
+@000265 DO ~SetGlobal("ux_banter_1", "GLOBAL", 1) ~
+  == uxbzar @000265 DO ~~
+  == uxbolr @000266 DO ~~
+  == uxbzar @000267 DO ~~
+  == uxbolr @000268 DO ~~
+  == uxbzar @000269 DO ~~
 EXIT
 
-CHAIN IF ~Global("ux_banter_2", "GLOBAL", 0) !StateCheck("uxzar", CD_STATE_NOTVALID) InParty("uxana") See("uxana") !StateCheck("uxana", CD_STATE_NOTVALID) InParty("uxath") See("uxath") !StateCheck("uxath", CD_STATE_NOTVALID)~ THEN uxbzar ux_lbl_chain_2
-@000255 DO ~SetGlobal("ux_banter_2", "GLOBAL", 1) ~
+CHAIN IF ~Global("ux_banter_2", "GLOBAL", 0) !StateCheck("uxzar", CD_STATE_NOTVALID) InParty("uxzar") See("uxzar") !StateCheck("uxzar", CD_STATE_NOTVALID) InParty("uxolr") See("uxolr") !StateCheck("uxolr", CD_STATE_NOTVALID)~ THEN uxbzar ux_lbl_chain_2
+@000271 DO ~SetGlobal("ux_banter_2", "GLOBAL", 1) ~
+  == uxbzar @000271 DO ~~
+  == uxbolr @000272 DO ~~
+  == uxbzar @000297 DO ~~
+  == uxbolr @000274 DO ~~
+  == uxbzar @000275 DO ~~
+EXIT
+
+CHAIN IF ~Global("ux_banter_3", "GLOBAL", 0) !StateCheck("uxzar", CD_STATE_NOTVALID) InParty("uxzar") See("uxzar") !StateCheck("uxzar", CD_STATE_NOTVALID) InParty("uxath") See("uxath") !StateCheck("uxath", CD_STATE_NOTVALID)~ THEN uxbzar ux_lbl_chain_3
+@000276 DO ~SetGlobal("ux_banter_3", "GLOBAL", 1) ~
+  == uxbzar @000276 DO ~~
+  == uxbath @000277 DO ~~
+  == uxbzar @000278 DO ~~
+  == uxbath @000279 DO ~~
+EXIT
+
+CHAIN IF ~Global("ux_banter_4", "GLOBAL", 0) !StateCheck("uxzar", CD_STATE_NOTVALID) InParty("uxzar") See("uxzar") !StateCheck("uxzar", CD_STATE_NOTVALID) InParty("uxath") See("uxath") !StateCheck("uxath", CD_STATE_NOTVALID)~ THEN uxbzar ux_lbl_chain_4
+@000280 DO ~SetGlobal("ux_banter_4", "GLOBAL", 1) ~
+  == uxbzar @000280 DO ~~
+  == uxbath @000281 DO ~~
+  == uxbzar @000282 DO ~~
+  == uxbath @000283 DO ~~
+  == uxbzar @000284 DO ~~
+  == uxbath @000285 DO ~~
+  == uxbzar @000286 DO ~~
+EXIT
+
+CHAIN IF ~Global("ux_banter_5", "GLOBAL", 0) !StateCheck("uxana", CD_STATE_NOTVALID) InParty("uxana") See("uxana") !StateCheck("uxana", CD_STATE_NOTVALID) InParty("uxzar") See("uxzar") !StateCheck("uxzar", CD_STATE_NOTVALID)~ THEN uxbana ux_lbl_chain_5
+@000298 DO ~SetGlobal("ux_banter_5", "GLOBAL", 1) ~
+  == uxbana @000298 DO ~~
+  == uxbzar @000299 DO ~~
+  == uxbana @000300 DO ~~
+  == uxbzar @000301 DO ~~
+  == uxbana @000302 DO ~~
+  == uxbzar @000303 DO ~~
+EXIT
+
+CHAIN IF ~Global("ux_banter_6", "GLOBAL", 0) !StateCheck("uxzar", CD_STATE_NOTVALID) InParty("uxzar") See("uxzar") !StateCheck("uxzar", CD_STATE_NOTVALID) InParty("uxana") See("uxana") !StateCheck("uxana", CD_STATE_NOTVALID)~ THEN uxbzar ux_lbl_chain_6
+@000304 DO ~SetGlobal("ux_banter_6", "GLOBAL", 1) ~
+  == uxbzar @000304 DO ~~
+  == uxbana @000305 DO ~~
+  == uxbzar @000306 DO ~~
+  == uxbzar @000307 DO ~~
+  == uxbana @000308 DO ~~
+  == uxbzar @000309 DO ~~
+  == uxbana @000310 DO ~~
+  == uxbzar @000311 DO ~~
+  == uxbana @000312 DO ~~
+EXIT
+
+CHAIN IF ~Global("ux_banter_7", "GLOBAL", 0) !StateCheck("uxana", CD_STATE_NOTVALID) InParty("uxana") See("uxana") !StateCheck("uxana", CD_STATE_NOTVALID) InParty("uxzar") See("uxzar") !StateCheck("uxzar", CD_STATE_NOTVALID) InParty("uxolr") See("uxolr") !StateCheck("uxolr", CD_STATE_NOTVALID)~ THEN uxbana ux_lbl_chain_7
+@000287 DO ~SetGlobal("ux_banter_7", "GLOBAL", 1) ~
+  == uxbana @000287 DO ~~
+  == uxbzar @000288 DO ~~
+  == uxbolr @000289 DO ~~
+  == uxbana @000290 DO ~~
+  == uxbzar @000291 DO ~~
+  == uxbana @000292 DO ~~
+  == uxbolr @000293 DO ~~
+  == uxbzar @000294 DO ~~
+  == uxbana @000295 DO ~~
+  == uxbolr @000296 DO ~~
+EXIT
+
+CHAIN IF ~Global("ux_banter_8", "GLOBAL", 0) !StateCheck("uxzar", CD_STATE_NOTVALID) InParty("uxana") See("uxana") !StateCheck("uxana", CD_STATE_NOTVALID) InParty("uxath") See("uxath") !StateCheck("uxath", CD_STATE_NOTVALID)~ THEN uxbzar ux_lbl_chain_8
+@000255 DO ~SetGlobal("ux_banter_8", "GLOBAL", 1) ~
   == uxbana @000251 DO ~~
   == uxbath @000252 DO ~~
   == uxbzar @000253 DO ~~
   == uxbana @000254 DO ~~
 EXIT
 
-CHAIN IF ~Global("ux_banter_3", "GLOBAL", 0) !StateCheck("uxath", CD_STATE_NOTVALID) InParty("uxana") See("uxana") !StateCheck("uxana", CD_STATE_NOTVALID)~ THEN uxbath ux_lbl_chain_3
-@000221 DO ~SetGlobal("ux_banter_3", "GLOBAL", 1) ~
+CHAIN IF ~Global("ux_banter_9", "GLOBAL", 0) !StateCheck("uxath", CD_STATE_NOTVALID) InParty("uxana") See("uxana") !StateCheck("uxana", CD_STATE_NOTVALID)~ THEN uxbath ux_lbl_chain_9
+@000221 DO ~SetGlobal("ux_banter_9", "GLOBAL", 1) ~
   == uxbana @000220 DO ~~
 EXIT
 
-CHAIN IF ~Global("ux_banter_4", "GLOBAL", 0) !StateCheck("uxolr", CD_STATE_NOTVALID) InParty("uxzar") See("uxzar") !StateCheck("uxzar", CD_STATE_NOTVALID)~ THEN uxbolr ux_lbl_chain_4
-@000223 DO ~SetGlobal("ux_banter_4", "GLOBAL", 1) ~
+CHAIN IF ~Global("ux_banter_10", "GLOBAL", 0) !StateCheck("uxolr", CD_STATE_NOTVALID) InParty("uxzar") See("uxzar") !StateCheck("uxzar", CD_STATE_NOTVALID)~ THEN uxbolr ux_lbl_chain_10
+@000223 DO ~SetGlobal("ux_banter_10", "GLOBAL", 1) ~
   == uxbzar @000222 DO ~~
 EXIT
 
-CHAIN IF ~Global("ux_banter_5", "GLOBAL", 0) !StateCheck("uxvoi", CD_STATE_NOTVALID) InParty("uxath") See("uxath") !StateCheck("uxath", CD_STATE_NOTVALID)~ THEN uxbvoi ux_lbl_chain_5
-@000226 DO ~SetGlobal("ux_banter_5", "GLOBAL", 1) ~
+CHAIN IF ~Global("ux_banter_11", "GLOBAL", 0) !StateCheck("uxvoi", CD_STATE_NOTVALID) InParty("uxath") See("uxath") !StateCheck("uxath", CD_STATE_NOTVALID)~ THEN uxbvoi ux_lbl_chain_11
+@000226 DO ~SetGlobal("ux_banter_11", "GLOBAL", 1) ~
   == uxbath @000224 DO ~~
   == uxbvoi @000225 DO ~~
 EXIT
 
-CHAIN IF ~Global("ux_banter_6", "GLOBAL", 0) !StateCheck("uxath", CD_STATE_NOTVALID) InParty("uxana") See("uxana") !StateCheck("uxana", CD_STATE_NOTVALID)~ THEN uxbath ux_lbl_chain_6
-@000235 DO ~SetGlobal("ux_banter_6", "GLOBAL", 1) ~
+CHAIN IF ~Global("ux_banter_12", "GLOBAL", 0) !StateCheck("uxath", CD_STATE_NOTVALID) InParty("uxana") See("uxana") !StateCheck("uxana", CD_STATE_NOTVALID)~ THEN uxbath ux_lbl_chain_12
+@000235 DO ~SetGlobal("ux_banter_12", "GLOBAL", 1) ~
   == uxbana @000227 DO ~~
   == uxbath @000228 DO ~~
   == uxbana @000229 DO ~~
@@ -41,32 +108,39 @@ CHAIN IF ~Global("ux_banter_6", "GLOBAL", 0) !StateCheck("uxath", CD_STATE_NOTVA
   == uxbath @000234 DO ~~
 EXIT
 
-CHAIN IF ~Global("ux_banter_7", "GLOBAL", 0) !StateCheck("uxath", CD_STATE_NOTVALID) InParty("uxana") See("uxana") !StateCheck("uxana", CD_STATE_NOTVALID)~ THEN uxbath ux_lbl_chain_7
-@000235 DO ~SetGlobal("ux_banter_7", "GLOBAL", 1) ~
+CHAIN IF ~Global("ux_banter_13", "GLOBAL", 0) !StateCheck("uxath", CD_STATE_NOTVALID) InParty("uxana") See("uxana") !StateCheck("uxana", CD_STATE_NOTVALID)~ THEN uxbath ux_lbl_chain_13
+@000235 DO ~SetGlobal("ux_banter_13", "GLOBAL", 1) ~
   == uxbana @000227 DO ~~
   == uxbath @000232 DO ~~
   == uxbana @000233 DO ~~
   == uxbath @000237 DO ~~
 EXIT
 
-CHAIN IF ~Global("ux_banter_8", "GLOBAL", 0) !StateCheck("uxana", CD_STATE_NOTVALID) InParty("uxzar") See("uxzar") !StateCheck("uxzar", CD_STATE_NOTVALID)~ THEN uxbana ux_lbl_chain_8
-@000241 DO ~SetGlobal("ux_banter_8", "GLOBAL", 1) ~
+CHAIN IF ~Global("ux_banter_14", "GLOBAL", 0) !StateCheck("uxana", CD_STATE_NOTVALID) InParty("uxzar") See("uxzar") !StateCheck("uxzar", CD_STATE_NOTVALID)~ THEN uxbana ux_lbl_chain_14
+@000241 DO ~SetGlobal("ux_banter_14", "GLOBAL", 1) ~
   == uxbzar @000238 DO ~~
   == uxbana @000239 DO ~~
   == uxbzar @000240 DO ~~
 EXIT
 
-CHAIN IF ~Global("ux_banter_9", "GLOBAL", 0) !StateCheck("uxath", CD_STATE_NOTVALID) InParty("uxolr") See("uxolr") !StateCheck("uxolr", CD_STATE_NOTVALID)~ THEN uxbath ux_lbl_chain_9
-@000245 DO ~SetGlobal("ux_banter_9", "GLOBAL", 1) ~
+CHAIN IF ~Global("ux_banter_15", "GLOBAL", 0) !StateCheck("uxath", CD_STATE_NOTVALID) InParty("uxolr") See("uxolr") !StateCheck("uxolr", CD_STATE_NOTVALID)~ THEN uxbath ux_lbl_chain_15
+@000245 DO ~SetGlobal("ux_banter_15", "GLOBAL", 1) ~
   == uxbolr @000242 DO ~~
   == uxbath @000243 DO ~~
   == uxbolr @000244 DO ~~
 EXIT
 
-CHAIN IF ~Global("ux_banter_10", "GLOBAL", 0) !StateCheck("uxvoi", CD_STATE_NOTVALID) InParty("uxath") See("uxath") !StateCheck("uxath", CD_STATE_NOTVALID) InParty("uxana") See("uxana") !StateCheck("uxana", CD_STATE_NOTVALID) InParty("uxolr") See("uxolr") !StateCheck("uxolr", CD_STATE_NOTVALID) InParty("uxzar") See("uxzar") !StateCheck("uxzar", CD_STATE_NOTVALID)~ THEN uxbvoi ux_lbl_chain_10
-@000250 DO ~SetGlobal("ux_banter_10", "GLOBAL", 1) ~
+CHAIN IF ~Global("ux_banter_16", "GLOBAL", 0) !StateCheck("uxvoi", CD_STATE_NOTVALID) InParty("uxath") See("uxath") !StateCheck("uxath", CD_STATE_NOTVALID) InParty("uxana") See("uxana") !StateCheck("uxana", CD_STATE_NOTVALID) InParty("uxolr") See("uxolr") !StateCheck("uxolr", CD_STATE_NOTVALID) InParty("uxzar") See("uxzar") !StateCheck("uxzar", CD_STATE_NOTVALID)~ THEN uxbvoi ux_lbl_chain_16
+@000250 DO ~SetGlobal("ux_banter_16", "GLOBAL", 1) ~
   == uxbath @000246 DO ~~
   == uxbana @000247 DO ~~
   == uxbolr @000248 DO ~~
   == uxbzar @000249 DO ~~
+EXIT
+
+CHAIN IF ~Global("ux_banter_17", "GLOBAL", 0) !StateCheck("uxzar", CD_STATE_NOTVALID) InParty("uxzar") See("uxzar") !StateCheck("uxzar", CD_STATE_NOTVALID) InParty("uxana") See("uxana") !StateCheck("uxana", CD_STATE_NOTVALID) InParty("uxolr") See("uxolr") !StateCheck("uxolr", CD_STATE_NOTVALID) !Global("ux_test", "GLOBAL", 1)~ THEN uxbzar ux_lbl_chain_17
+@000261 DO ~SetGlobal("ux_banter_17", "GLOBAL", 1) SetGlobal("ux_test", "GLOBAL", 1)~
+  == uxbzar @000261 DO ~SetGlobal("ux_test", "GLOBAL", 1)~
+  == uxbana @000262 DO ~~
+  == uxbolr @000263 DO ~~
 EXIT
